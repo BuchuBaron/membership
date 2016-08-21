59c65306fa0d638dd5225636fa22b7e1a3a7f0f1
Charles Oertel <charles.oertel@rocketseed.com>
2016-08-21 14:52:04 +0200

    jQuery actions done.  Now do drupal paths to implement the commands and the filters.

--- membership-admin.js
+++ membership-admin.js
@@ -1,21 +1,56 @@
 Drupal.behaviors.membership_admin = function(context) {
-  $('body:not(.set_logo_click_handler)').addClass('set_logo_click_handler').each(function() {
-    //Remove Logo Click Handler
-    $('#remove_logo').click(function(){
-      var account_id_in = ACCOUNT_ID;
+  // Set up click events on the display selector checkboxes.
+  $.each(['status', 'identity', 'contact', 'emergency'], function(index, value) {
+    $('#' + value, context).click(function() {
+      if ($(this).attr('checked')) {
+        $('.' + value).show();
+      }
+      else {
+        $('.' + value).hide();
+      }
+    });
+    $('.status').hide();
+    $('.emergency').hide();
+    $('#contact').attr('checked', 'checked');
+    $('#identity').attr('checked', 'checked');
+  });
 
-      var post_data = {
-        account_id : account_id_in
-      };
+  // Set up select-all
+  $('#select-all', context).click(function() {
+    if ($(this).attr('checked')) {
+      $('.selection').attr('checked', 'checked');
+    }
+    else {
+      $('.selection').removeAttr('checked');
+    }
+  });
 
-      $.post(BASE_URL + 'rsaccountadmin/edit/remove_image/' + account_id_in, post_data, (function(account_id_in) {
-        //Removing logo image and asscociated button
-        $('#rsaccountadmin-account-edit-form div').find('img').remove();
-        $('#remove_logo').remove();
-      }), 'json');
+  // Set up unselect all when one checkbox is unselected.
+  $('.selection', context).click(function() {
+    if (!$(this).attr('checked')) {
+      $('#select-all').removeAttr('checked');
+    }
+  });
 
+  // Set up the actions on selected records.
+  $('#run-action', context).click(function() {
+    var command = $('#action').val();
+    // Assemble a list of ids to send to the server with the command:
+    var selected_members = $.map($('.selection:checked', context), function(element) {
+      return element.value;
+    });
+    alert("Running " + command + " on selection of " + selected_members.join(','));
+    var post_data = {
+      // filter: filter,
+      selected_members: selected_members
+    };
+
+    $.post('/membership/update/' + command, post_data, function(result) {
+      if (result === 'no landingpages') {
+        alert('Really?!');
+      }
+      else {
+        alert("Ran " + command + " on " + selected_members.join(','));
     });
   });
 };
-
-
