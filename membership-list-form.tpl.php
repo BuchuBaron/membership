a2f76315fca4a7358bae741900479db731fe1bf9
Charles Oertel <charles.oertel@rocketseed.com>
2016-09-04 22:14:00 +0200

    Added bike field, now formatting layout of admin filter.  Remaining data sanitization, formatting registration screen, and allowing users to edit their details.

--- membership-list-form.tpl.php
+++ membership-list-form.tpl.php
@@ -10,5 +10,31 @@
 if (!empty($q)):
   print $q;
 endif;
-
+?>
+<style>
+label {
+  clear: left;
+}
+label.option {
+  float: left;
+  clear: none;
+}
+div.description {
+  clear: left;
+}
+</style>
+<table style="border:none"><tr>
+<td>
+<?php
+print drupal_render($form['Filter']['member_number']);
+print drupal_render($form['Filter']['firstname']);
+print drupal_render($form['Filter']['surname']);
+print drupal_render($form['Filter']['email']);
+print drupal_render($form['Filter']['bike']);
+print "\n</td><td>\n";
+print drupal_render($form['Filter']['status']);
+print drupal_render($form['Filter']['paid']);
+print drupal_render($form['Filter']['gender']);
+print drupal_render($form['Filter']['card_collection']);
+print "\n</td></tr></table>";
 print drupal_render($form);
